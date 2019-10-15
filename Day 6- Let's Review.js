9490810c1b273d1a15f535289f9d5a43b62bd88c
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-10-14 22:01:05 -0400

    even0 appear undefined why?

--- Day 6- Let's Review.js
+++ Day 6- Let's Review.js
@@ -2,17 +2,31 @@ let str = 'Hacker';
 let result = str.split('');
 console.log(result);
 result.forEach((element, index) => {
-	console.log(index, element);
+	// console.log(index, element);
 	if (index % 2 === 0) console.log(element);
 	if (!index % 2 === 0) console.log(element);
 });
 
-function review() {
-	console.log(`${even0} ${odd0}
-    ${even1} ${odd1}`);
+function review(value, str0, str1) {
+	value = 2;
+	str0 = 'Hacker';
+	str1 = 'Rank';
+	let firstSplit = str0.split('');
+	console.log(firstSplit);
+	let even0 = firstSplit.forEach((element, index) => {
+		if (index % 2 === 0) console.log(element);
+	});
+	console.log(
+		firstSplit.forEach((element, index) => {
+			if (index % 2 === 0) console.log(element);
+		})
+	);
+	console.log(even0);
 }
 
-var x = 'HELLO WORLD';
-var res = x.charAt(0);
-var res = x.charAt(1);
-console.log(res);
+review(2, 'Hacker', 'Rank');
+
+// var x = 'HELLO WORLD';
+// var res = x.charAt(0);
+// var res = x.charAt(1);
+// console.log(res);
